99890e17f9d20fd6f95288d460f17a9dfe203cf2
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2019-10-11 08:05:47 -0700

    changed api url

--- src/graphqlApollo/apolloClient.js
+++ src/graphqlApollo/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   // uri: "https://image-repo-api.herokuapp.com"
-  uri: "http://localhost:4000/"
+  uri: "https://image-repo-api.herokuapp.com/"
 });
 
 export default client;
